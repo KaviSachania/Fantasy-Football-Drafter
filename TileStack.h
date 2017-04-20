ca0e91b1bd7687cb959f838d3a2aad9f9b60bbb0
Kavi Sachania <kavi.sachania@gmail.com>
2017-04-19 21:29:15 -0700

    Update TileStack.h

--- TileStack.h
+++ TileStack.h
@@ -1,8 +1,4 @@
-// This is the .h file you will edit and turn in.
-// We have provided a skeleton for you,
-// but you must finish it as described in the spec.
-// Also remove these comments here and add your own.
-// TODO: remove this comment header
+
 
 #ifndef _tilestack_h
 #define _tilestack_h
